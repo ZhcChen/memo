8049c2f39008f90814d98f385bb66c2edbe4beee
陈智聪 <1406736605@qq.com>
2022-11-09 14:29:45 +0800

    update

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -71,7 +71,7 @@ module.exports = {
       //   ]
       // },
       { text: '笔记', link: '/pages/d9e60d/' },
-      { text: '资源', link: '/pages/20d224/' }
+      { text: '书签', link: '/pages/20d224/' }
     ],
     sidebarDepth: 2, // 侧边栏显示深度，默认1，最大2（显示到h3标题）
     logo: '/img/logo.png', // 导航栏logo
